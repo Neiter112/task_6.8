e1fb796f5a44c95fad7c8354671ab3f4719fe970
Roman Baryshnikov <neiter.nox@gmail.com>
2024-11-22 13:57:22 +0300

    First commit

---
+++ Task2/script.js
@@ -0,0 +1,15 @@
+const conlog = document.querySelector('#conlog');
+conlog.addEventListener('click', function() {
+     alert('console.log() — это метод в JavaScript, который используется для вывода сообщений в консоль. ');
+})
+
+const ar = document.querySelector('#ar');
+ar.addEventListener('click', function() {
+     alert('Функция alert в JavaScript выводит в браузере предупреждающее модальное диалоговое окно с сообщением и кнопкой «ОК»');
+})
+
+const pt = document.querySelector('#pt');
+pt.addEventListener('click', function() {
+     alert('prompt() — это метод объекта window в JavaScript, который выводит диалоговое окно с сообщением, текстовым полем для ввода данных и кнопками «ОК» и «Отмена».');
+})
+const result = prompt(message, default) (https://itchief.ru/javascript/alert-prompt-confirm));
