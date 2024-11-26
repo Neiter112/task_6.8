90ab5a610b6d42e21cb6f039060f67b19b3c865b
Roman Baryshnikov <neiter.nox@gmail.com>
2024-11-26 13:52:20 +0300

    app

--- task4/script.js
+++ task4/script.js
@@ -1,6 +1,6 @@
 change.addEventListener('click', function (event) {
     event.preventDefault();
-    const userText = prompt('Что вы хотите от меня?');
+    const userText = prompt('Напишите текст и посмотрите, что случится.');
     const change = document.querySelector('#change');
     change.textContent = userText;
 })
